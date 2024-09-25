cfcf97a9050e353acf003c298e3293261f0dc4a9
keedar <50590064@qq.com>
2024-09-25 10:35:30 +0800

    11

--- tail.go
+++ tail.go
@@ -7,6 +7,7 @@ import (
 	"bufio"
 	"errors"
 	"fmt"
+	"gopkg.in/tomb.v1"
 	"io"
 	"io/ioutil"
 	"log"
@@ -15,10 +16,9 @@ import (
 	"sync"
 	"time"
 
-	"github.com/hpcloud/tail/ratelimiter"
-	"github.com/hpcloud/tail/util"
-	"github.com/hpcloud/tail/watch"
-	"gopkg.in/tomb.v1"
+	"github.com/hnlylmlzh/tail/ratelimiter"
+	"github.com/hnlylmlzh/tail/util"
+	"github.com/hnlylmlzh/tail/watch"
 )
 
 var (
